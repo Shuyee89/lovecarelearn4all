c706c1a64fc6284597b0bc9dee5cd0299d9781e9
Deena Lim <deena.lsy7@gmail.com>
2024-04-04 23:42:45 +0800

    Test NRIC details

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 export const Profile: React.FC = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+  const [NRIC, setNRIC] = useState(null);
   useEffect(() => {
     const getMessage = async () => {
       const code = queryParams.get("code");
       const url = `/.netlify/functions/getIDToken?code=${code}`;
       const { data } = await axios.get(url);
-      console.log(data.data);
+      setNRIC(data.data);
     };
 
     getMessage();
   });
 
-  return <div>Profile</div>;
+  return <div>Hello, {NRIC}</div>;
 };
 
 export default Profile;
