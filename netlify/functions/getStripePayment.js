9a9c316efd7bd1466763d0db5975fec50a8900bf
Deena Lim <deena.lsy7@gmail.com>
2024-04-12 16:41:07 +0800

    Commit function changes

--- netlify/functions/getStripePayment.js
+++ netlify/functions/getStripePayment.js
@@ -1,31 +1,36 @@
-
 const stripe = require("stripe")(
-    "sk_test_51OZwE7Jr15PbubHRg7Mofo6zB4xB1oLmmIExy8XXB36ZPgCUy0mOzX4TKwkiZtBpp0Y0j6MmqsVQdw2XdJGPJypY00ykvzMZne"
-  );;
+  "sk_test_51OZwE7Jr15PbubHRg7Mofo6zB4xB1oLmmIExy8XXB36ZPgCUy0mOzX4TKwkiZtBpp0Y0j6MmqsVQdw2XdJGPJypY00ykvzMZne"
+);
+const YOUR_DOMAIN = "https://lovecarelearn4all.netlify.app";
 
 exports.handler = async (event, context) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
+  try {
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: "embedded",
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: "price_1P4dDOJr15PbubHRzuTd5RNK",
+          quantity: 1,
         },
-        quantity: 1,
+      ],
+      mode: "payment",
+      return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: session.client_secret }),
+      headers: {
+        "Content-Type": "application/json",
       },
-    ],
-    mode: "payment",
-    success_url: "https://serverless-payments.netlify.app/success",
-    cancel_url: "https://serverless-payments.netlify.app/cancel",
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      id: session.id,
-    }),
-  };
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ data: e }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  }
 };
